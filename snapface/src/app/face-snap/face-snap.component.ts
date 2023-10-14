a79a0bd6c4ae28d3f537b4854ea65ea87d53a5ba
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-10-14 11:13:17 +0200

    init angular & add first component

---
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss'],
+})
+export class FaceSnapComponent {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+
+  ngOnInit() {
+    this.title = 'Etienne';
+    this.description = "L'amour de ma vie";
+    this.createdDate = new Date();
+    this.snaps = 8;
+    this.imageUrl =
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
+}
