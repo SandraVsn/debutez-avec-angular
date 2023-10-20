564299777590a865cc407368d60c2b421caac655
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-10-20 12:20:03 +0200

    Add Routing Module

--- snapface/src/app/app.module.ts
+++ snapface/src/app/app.module.ts
@@ -1,30 +1,27 @@
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { registerLocaleData } from '@angular/common'
-import * as fr from '@angular/common/locales/fr'
+import { registerLocaleData } from '@angular/common';
+import * as fr from '@angular/common/locales/fr';
 
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     FaceSnapComponent,
     FaceSnapListComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [
-    {provide: LOCALE_ID, useValue: 'fr-FR'}
-  ],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { 
-  constructor(){
-    registerLocaleData(fr.default)
+export class AppModule {
+  constructor() {
+    registerLocaleData(fr.default);
   }
 }
