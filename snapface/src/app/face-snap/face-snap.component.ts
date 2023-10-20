0491dea39ba92e140b2dc01dfb2e5b6d27bda5f9
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-10-20 11:23:43 +0200

    Add method in service to snap

--- snapface/src/app/face-snap/face-snap.component.ts
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapsService } from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap',
@@ -11,13 +12,15 @@ export class FaceSnapComponent {
 
   buttonText!: string;
 
+  constructor(private faceSnapsService: FaceSnapsService) {}
+
   ngOnInit() {
     this.buttonText = 'Oh Snap!';
   }
 
   onSnap() {
     if (this.buttonText === 'Oh Snap!') {
-      this.faceSnap.snaps++;
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id);
       this.buttonText = 'Oops, unSnap!';
     } else {
       this.faceSnap.snaps--;
