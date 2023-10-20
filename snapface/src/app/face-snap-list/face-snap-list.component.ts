084fbbdf0f216033515f87d94705a62aa57cec79
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-10-20 11:09:50 +0200

    Inject and Use the service

--- snapface/src/app/face-snap-list/face-snap-list.component.ts
+++ snapface/src/app/face-snap-list/face-snap-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapsService } from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -8,35 +9,10 @@ import { FaceSnap } from '../models/face-snap.model';
 })
 export class FaceSnapListComponent {
   faceSnaps!: FaceSnap[];
+
+  constructor(private faceSnapsService: FaceSnapsService){}
   
   ngOnInit(): void {
-    this.faceSnaps = [
-       {
-        title: 'Etienne',
-        description: `L'amour de ma vie`,
-        snaps: 60,
-        imageUrl:
-          'https://images.pexels.com/photos/18536009/pexels-photo-18536009/free-photo-of-noir-et-blanc-mode-homme-hiver.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-        createdDate: new Date(),
-        location: 'Bordeaux',
-      },
-       {
-        title: 'Elise',
-        description: `La meilleure amie de l'univers`,
-        snaps: 40,
-        imageUrl:
-          'https://images.pexels.com/photos/10456227/pexels-photo-10456227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-        createdDate: new Date(),
-      },
-      {
-        title: 'Guillaume',
-        description: 'Le meilleur des amis',
-        snaps: 6,
-        imageUrl:
-          'https://images.pexels.com/photos/18360787/pexels-photo-18360787/free-photo-of-homme-fleurs-blond-debout.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-        createdDate: new Date(),
-        location: 'Hambourg',
-      }
-    ]
+    this.faceSnaps = this.faceSnapsService.faceSnaps
   }
 }
