716c4fd5e109b7e19a033bfbceaac7cd4d770e2e
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-10-20 14:04:36 +0200

    fix: use paramId to get single faceSnap to display

--- snapface/src/app/single-face-snap/single-face-snap.component.ts
+++ snapface/src/app/single-face-snap/single-face-snap.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-single-face-snap',
@@ -8,14 +9,19 @@ import { FaceSnapsService } from '../services/face-snaps.service';
   styleUrls: ['./single-face-snap.component.scss'],
 })
 export class SingleFaceSnapComponent {
-  @Input() faceSnap!: FaceSnap;
+  faceSnap!: FaceSnap;
 
   buttonText!: string;
 
-  constructor(private faceSnapsService: FaceSnapsService) {}
+  constructor(
+    private faceSnapsService: FaceSnapsService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.buttonText = 'Oh Snap!';
+    const faceSnapId = +this.route.snapshot.params['id'];
+    this.faceSnap = this.faceSnapsService.getFaceSnapById(faceSnapId);
   }
 
   onSnap() {
