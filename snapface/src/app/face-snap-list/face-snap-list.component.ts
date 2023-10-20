37dc525745116389d8d0708bb6af6f9ad0a20e5f
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-10-20 16:31:39 +0200

    Create a memory leak

--- snapface/src/app/face-snap-list/face-snap-list.component.ts
+++ snapface/src/app/face-snap-list/face-snap-list.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-face-snap-list',
   templateUrl: './face-snap-list.component.html',
-  styleUrls: ['./face-snap-list.component.scss']
+  styleUrls: ['./face-snap-list.component.scss'],
 })
 export class FaceSnapListComponent {
   faceSnaps!: FaceSnap[];
 
-  constructor(private faceSnapsService: FaceSnapsService){}
-  
+  constructor(private faceSnapsService: FaceSnapsService) {}
+
   ngOnInit(): void {
-    this.faceSnaps = this.faceSnapsService.getAllFaceSnaps()
+    this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
+
+    interval(1000).pipe(tap(console.log)).subscribe();
   }
 }
