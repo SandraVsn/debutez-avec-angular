5571d016a0b21b22f730c0c3e6f6b28401a85f09
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-10-23 22:15:50 +0200

    Fix: snap function

--- snapface/src/app/services/face-snaps.service.ts
+++ snapface/src/app/services/face-snaps.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FaceSnapsService {
   constructor(private http: HttpClient) {}
-
-  faceSnaps: FaceSnap[] = [];
 
   getAllFaceSnaps(): Observable<FaceSnap[]> {
     return this.http.get<FaceSnap[]>('http://localhost:3000/facesnaps');
@@ -19,9 +17,22 @@ export class FaceSnapsService {
     return this.http.get<FaceSnap>(`http://localhost:3000/facesnaps/${id}`);
   }
 
-  snapFaceSnapById(id: number, snapType: 'snap' | 'unsnap'): void {
-    // const faceSnap = this.getFaceSnapById(id);
-    // snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
+  snapFaceSnapById(
+    id: number,
+    snapType: 'snap' | 'unsnap'
+  ): Observable<FaceSnap> {
+    return this.getFaceSnapById(id).pipe(
+      map((faceSnap) => ({
+        ...faceSnap,
+        snaps: faceSnap.snaps + (snapType === 'snap' ? 1 : -1),
+      })),
+      switchMap((updtatedFaceSnap) =>
+        this.http.put<FaceSnap>(
+          `http://localhost:3000/facesnaps/${id}`,
+          updtatedFaceSnap
+        )
+      )
+    );
   }
 
   addFaceSnap(formValue: {
@@ -30,12 +41,12 @@ export class FaceSnapsService {
     imageUrl: string;
     location?: string;
   }): void {
-    const faceSnap: FaceSnap = {
-      ...formValue,
-      createdDate: new Date(),
-      snaps: 0,
-      id: this.faceSnaps[this.faceSnaps.length - 1].id + 1,
-    };
-    this.faceSnaps.push(faceSnap);
+    // const faceSnap: FaceSnap = {
+    //   ...formValue,
+    //   createdDate: new Date(),
+    //   snaps: 0,
+    //   id: this.faceSnaps[this.faceSnaps.length - 1].id + 1,
+    // };
+    // this.faceSnaps.push(faceSnap);
   }
 }
--- snapface/src/app/single-face-snap/single-face-snap.component.ts
+++ snapface/src/app/single-face-snap/single-face-snap.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-single-face-snap',
@@ -25,13 +25,21 @@ export class SingleFaceSnapComponent {
     this.faceSnap$ = this.faceSnapsService.getFaceSnapById(faceSnapId);
   }
 
-  onSnap() {
-    // if (this.buttonText === 'Oh Snap!') {
-    //   this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap');
-    //   this.buttonText = 'Oops, unSnap!';
-    // } else {
-    //   this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
-    //   this.buttonText = 'Oh Snap!';
-    // }
+  onSnap(id: number) {
+    if (this.buttonText === 'Oh Snap!') {
+      this.faceSnap$ = this.faceSnapsService.snapFaceSnapById(id, 'snap').pipe(
+        tap(() => {
+          this.buttonText = 'Oops, unSnap!';
+        })
+      );
+    } else {
+      this.faceSnap$ = this.faceSnapsService
+        .snapFaceSnapById(id, 'unsnap')
+        .pipe(
+          tap(() => {
+            this.buttonText = 'Oh Snap!';
+          })
+        );
+    }
   }
 }
