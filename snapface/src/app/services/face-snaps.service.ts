f28343adf045cbd506fe1c7ba6cd7600f1aee840
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-10-23 14:20:50 +0200

    Create first request

--- snapface/src/app/services/face-snaps.service.ts
+++ snapface/src/app/services/face-snaps.service.ts
@@ -1,44 +1,18 @@
 import { Injectable } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FaceSnapsService {
-  faceSnaps: FaceSnap[] = [
-    {
-      id: 1,
-      title: 'Etienne',
-      description: `L'amour de ma vie`,
-      snaps: 60,
-      imageUrl:
-        'https://images.pexels.com/photos/18536009/pexels-photo-18536009/free-photo-of-noir-et-blanc-mode-homme-hiver.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-      createdDate: new Date(),
-      location: 'Bordeaux',
-    },
-    {
-      id: 2,
-      title: 'Elise',
-      description: `La meilleure amie de l'univers`,
-      snaps: 40,
-      imageUrl:
-        'https://images.pexels.com/photos/10456227/pexels-photo-10456227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-      createdDate: new Date(),
-    },
-    {
-      id: 3,
-      title: 'Guillaume',
-      description: 'Le meilleur des amis',
-      snaps: 6,
-      imageUrl:
-        'https://images.pexels.com/photos/18360787/pexels-photo-18360787/free-photo-of-homme-fleurs-blond-debout.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-      createdDate: new Date(),
-      location: 'Hambourg',
-    },
-  ];
+  constructor(private http: HttpClient) {}
 
-  getAllFaceSnaps(): FaceSnap[] {
-    return this.faceSnaps;
+  faceSnaps: FaceSnap[] = [];
+
+  getAllFaceSnaps(): Observable<FaceSnap[]> {
+    return this.http.get<FaceSnap[]>('http://localhost:3000/facesnaps');
   }
 
   getFaceSnapById(id: number): FaceSnap {
