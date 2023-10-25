3081ccb3ffb516e276c09f890310ef9751b93305
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-10-25 21:31:37 +0200

    Lazy Load FaceSnap Feature Module

--- snapface/src/app/app.module.ts
+++ snapface/src/app/app.module.ts
@@ -2,21 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { FaceSnapsModule } from './face-snaps/face-snaps.module';
 import { LandingPageModule } from './landing-page/landing-page.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    FaceSnapsModule,
-    BrowserModule,
-    AppRoutingModule,
-    CoreModule,
-    LandingPageModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, LandingPageModule],
   providers: [],
   bootstrap: [AppComponent],
 })
